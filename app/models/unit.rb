617a5f860591cf618a47037bcc41254dd67ffba5
Mike Schwab <mike.schwab@gmail.com>
2015-02-18 20:31:04 -0500

    units have unique names within building and floor

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -3,6 +3,6 @@ class Unit < ActiveRecord::Base
   
   validates :building, presence: true
   validates :floor, numericality: {greater_than_or_equal_to: 1}, presence: true
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: {scope: [:building_id, :floor]}
   validates :bedrooms, numericality: {greater_than_or_equal_to: 1}, presence: true
 end
